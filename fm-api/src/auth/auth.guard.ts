d259452831c037d1b8b303868ab4070c31206351
boykovm <66530211+boykovm@users.noreply.github.com>
2025-01-08 01:18:02 +0300

    Update fm-api/src/auth/auth.guard.ts

--- fm-api/src/auth/auth.guard.ts
+++ fm-api/src/auth/auth.guard.ts
@@ -21,8 +21,6 @@ export class AuthGuard implements CanActivate {
     }
 
     try {
-      // 💡 We're assigning the payload to the request object here
-      // so that we can access it in our route handlers
       request['user'] = await this.jwtService.verifyAsync(token, {
         secret: jwtConstants.secret,
       });
